1cb4d57992c7ab697c7d3528d7e850517f0f6907
Gaurangi25 <gaurangi250704@gmail.com>
2025-07-02 04:38:17 +0530

    heading modified

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="heading-outer-div">
+      <img src="/image.png" alt="logo" className="icon" />
+      <h1 className="heading">Morse Forge</h1>
+    </header>
+  );
+}
+
+export default Header;
